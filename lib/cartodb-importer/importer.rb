1436dd61afe8779f0c59067f8af67554d1e5abc1
Fernando Blat <ferblape@gmail.com>
2011-06-16 17:00:32 +0200

    Create SHP file without using an auxiliary table

--- lib/cartodb-importer/importer.rb
+++ lib/cartodb-importer/importer.rb
@@ -138,17 +138,12 @@ module CartoDB
         host = @db_configuration[:host] ? "-h #{@db_configuration[:host]}" : ""
         port = @db_configuration[:port] ? "-p #{@db_configuration[:port]}" : ""
         @suggested_name = get_valid_name(File.basename(path).tr('.','_').downcase.sanitize) unless @force_name
-        random_name = "importing_table_#{@suggested_name}"
-        puts "Table name to import: #{random_name}"
-        command = `\`which python\` #{File.expand_path("../../../misc/shp_normalizer.py", __FILE__)} #{path} #{random_name}`
+        command = `\`which python\` #{File.expand_path("../../../misc/shp_normalizer.py", __FILE__)} #{path} #{@suggested_name}`
         if command.strip.blank?
-          raise "Error running python shp_normalizer script: \`which python\` #{File.expand_path("../../../misc/shp_normalizer.py", __FILE__)} #{path} #{random_name}"
+          raise "Error running python shp_normalizer script: \`which python\` #{File.expand_path("../../../misc/shp_normalizer.py", __FILE__)} #{path} #{@suggested_name}"
         end
         puts "Running shp2pgsql: #{command.strip} | `which psql` #{host} #{port} -U#{@db_configuration[:username]} -w -d #{@db_configuration[:database]}"
         system("#{command.strip} | `which psql` #{host} #{port} -U#{@db_configuration[:username]} -w -d#{@db_configuration[:database]}")
-        imported_schema = @db_connection[random_name.to_sym].columns
-        @db_connection.run("CREATE TABLE #{@suggested_name} AS SELECT #{imported_schema.join(',')} FROM #{random_name}")
-        @db_connection.run("DROP TABLE #{random_name}")
         @table_created = true
         
         entries.each{ |e| FileUtils.rm_rf(e) } if entries.any?
